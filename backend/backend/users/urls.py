f29b6bf5fc571055288f760ee1d7191cdd5194f7
deliasoare <deliasoare006@yahoo.com>
2025-04-22 16:19:17 +0300

    updates

--- backend/backend/users/urls.py
+++ backend/backend/users/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("login", views.login_view, name="login"),
     path("user_info", views.user_info, name="user_info"),
-    path("check_login", views.check_login_view, name="check_login")
+    path("check_login", views.check_login_view, name="check_login"),
+    path("add_wishlist", views.add_bucketlist, name="add_wishlist"),
+    path("add_journal", views.add_journal, name="add_journal"),
 ]
