b631ca77e8ad6625cc58bd3a594018289b7782a2
Radulescu Razvan <proxy.razvan@gmail.com>
2025-04-22 15:44:44 +0300

    .

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -3,6 +3,10 @@ export default function Menu({ country }) {
         <div className="backgroundMenu">
             <div className="countryMenu">
                 <h1>{country}</h1>
+
+                <button>Add to Bucketlist</button>
+                <button>Add to Travel Journal</button>
+
             </div>
         </div>
 
