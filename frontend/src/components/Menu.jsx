37f963166be1fb3eb672eec3a1e562f399618b10
Radulescu Razvan <proxy.razvan@gmail.com>
2025-04-26 12:38:53 +0300

    .

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useState } from "react";
 
 export default function Menu({ country }) {
-    const [generatedText, setGeneratedText] = useState(""); // State to store the generated text
+    // const [generatedText, setGeneratedText] = useState(""); // State to store the generated text
 
     const add_wishlist = (e) => {
         axios.post(ADD_WISHLIST_ENDPOINT_URL, {
@@ -33,18 +33,18 @@ export default function Menu({ country }) {
             });
     };
 
-    const generateText = async () => {
-        try {
-            const response = await axios.post(
-                "http://127.0.0.1:8000/generate_description/",
-                { country: country },
-                { headers: { "Content-Type": "application/json" } }
-            );
-            setGeneratedText(response.data.description); // Update the state with the generated text
-        } catch (error) {
-            console.error("Error generating text:", error);
-        }
-    };
+    // const generateText = async () => {
+    //     try {
+    //         const response = await axios.post(
+    //             "http://127.0.0.1:8000/generate_description/",
+    //             { country: country },
+    //             { headers: { "Content-Type": "application/json" } }
+    //         );
+    //         setGeneratedText(response.data.description); // Update the state with the generated text
+    //     } catch (error) {
+    //         console.error("Error generating text:", error);
+    //     }
+    // };
 
     return (
         <div className="backgroundMenu">
@@ -53,12 +53,12 @@ export default function Menu({ country }) {
 
                 <button onClick={(e) => add_wishlist(e)}>Add to Bucketlist</button>
                 <button onClick={(e) => add_journal(e)}>Add to Travel Journal</button>
-                <button onClick={generateText}>Generate Description</button>
+                {/* <button onClick={generateText}>Generate Description</button> */}
 
-                {/* Display the generated text below the country's name */}
+                {/* Display the generated text below the country's name
                 {generatedText && (
                     <p className="generated-text">{generatedText}</p>
-                )}
+                )} */}
             </div>
         </div>
     );
