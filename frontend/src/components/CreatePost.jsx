a55be546340b145b97a04d8e17d6745764a7493b
Radulescu Razvan <proxy.razvan@gmail.com>
2025-07-28 13:07:29 +0300

    .

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -1,4 +1,4 @@
-﻿import React, { useState, useRef } from 'react';
+﻿﻿import React, { useState, useRef } from 'react';
 import { useLanguage } from '../context/LanguageContext';
 import translations from '../utils/translations';
 import axios from 'axios';
